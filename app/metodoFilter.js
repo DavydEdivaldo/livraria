1275a9d45f0e3927d6166da9f0f3d1180a81390f
davyd <davydedivaldo@gmail.com>
2024-04-24 23:20:41 -0300

    metodo filter
    começando a implementar o metodo filter, para filtar os livros por categotias

---
+++ app/metodoFilter.js
@@ -0,0 +1,10 @@
+// const btnFiltrarLivrosFront = document.getElementById("btnFiltrarLivrosFront");
+// btnFiltrarLivrosFront.addEventListener('click', filtrarLivrosFront);
+
+// function filtrarLivrosFront(){
+//     let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end');
+//     console.table(livrosFiltrados)
+// }
+
+
+const btnfront = document.querySelectorAll('button')
