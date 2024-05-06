f0cb0d01edccf0805e56ba0e770b19d47091dc41
Davyd Silva <davydsilva724@gmail.com>
2024-05-06 19:17:07 -0300

    implementando o btn de filtrar por disponiveis
    dando logica ao botão disponiveis, usando uma condicional para mostrar apenas o livros com quantidade maior que 0

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -11,4 +11,15 @@ function filtrarLivrosFront(){
 
     let livrosFiltrados = categoria == "disponivel" ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
     exibirLivrosNaTela(livrosFiltrados)
+    if(categoria == "disponivel"){
+        exibirValorTotalDosLivrosDisponiveisNaTela()
+    }
+ }
+
+ function exibirValorTotalDosLivrosDisponiveisNaTela(){
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+  </div>
+    `
  }
